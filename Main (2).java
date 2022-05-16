79bd894457b3fbab7d419013bcca5d9d3052efcc
VIPUL KUMAR <54803468+vipulkumar5@users.noreply.github.com>
2022-05-16 17:47:11 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a first  value:");
+		int n=sc.nextInt();
+		System.out.println("enter a second vlue:");
+		int m=sc.nextInt();
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=m;j++){
+		        
+		            System.out.print("*");
+		        
+		      
+		        
+		    }
+		    System.out.println( );
+		}
+		
+	}
+}
